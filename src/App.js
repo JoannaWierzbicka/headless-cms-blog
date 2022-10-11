43f221b9ff37dd2406459cdff0092304d0d6d985
Joanna Wierzbicka <joannawierzbicka@poczta.onet.pl>
2022-10-11 13:40:48 +0200

    start with create-react-app and prismic

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      CodeRoad APP
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import { PrismicProvider } from '@prismicio/react'
+import { client } from './prismic'
+
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PrismicProvider client={client}>
+      <App />
+    </PrismicProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
